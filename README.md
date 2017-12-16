0deb02d1ed003785ab40872cdc2ad445f5be05fc
Guido.Oelmann <guido@oelmann.net>
2017-12-16 12:02:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Spring Boot Microservice
+
+Migration des Microservices aus Beispiel [spring-boot-microservice-java8](https://github.com/javaakademie/spring-boot-microservice-java8) hin zu Java 9 und einem Java 9 Modul.
+
+## Service starten
+
+Auf die schnelle (WEITERE ERKLÄRUNG/ANSATZ FOLGEN NOCH): <br/>
+`java -jar --add-modules ALL-DEFAULT,java.xml.bind target/comics-0.0.1-SNAPSHOT.jar`
+
+## Aufruf der Anwendung
+
+`http://localhost:8080/comics`<br/>
+`http://localhost:8080/comics/{id}`
+
+
+
---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module de.javaakademie.comic {
+	requires java.xml.bind;
+	requires javax.transaction.api;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.context;
+	requires spring.beans;
+	requires spring.core;
+	requires spring.tx;
+	requires spring.web;
+	requires spring.webmvc;
+	requires spring.data.commons;
+	requires spring.data.jpa;
+	requires hibernate.jpa;
+	requires hibernate.search.engine;
+	requires org.hibernate.validator;
+	requires java.validation;
+}
